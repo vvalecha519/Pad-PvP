90855fcf23b53428ad1308b97862f708eacb8bb1
cnguye18 <66088422+Cody-Newguyin@users.noreply.github.com>
2021-01-16 12:04:53 -0500

    Added Orb Entity base

---
+++ UbiGame_Blank/Source/Game/GameEntities/OrbEntity.cpp
@@ -0,0 +1,9 @@
+#include "OrbEntity.h"
+
+Game::OrbEntity::OrbEntity()
+{
+}
+
+Game::OrbEntity::~OrbEntity()
+{
+}
---
+++ UbiGame_Blank/Source/Game/GameEntities/OrbEntity.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "GameEngine/EntitySystem/Entity.h"
+#include "GameEngine/EntitySystem/Components/SpriteRenderComponent.h"
+
+namespace Game
+{
+	class OrbEntity : public GameEngine::Entity
+	{
+	public:
+		OrbEntity();
+		~OrbEntity();
+	
+	protected:
+		GameEngine::SpriteRenderComponent** m_renderComponent;
+	};
+}
+
+
