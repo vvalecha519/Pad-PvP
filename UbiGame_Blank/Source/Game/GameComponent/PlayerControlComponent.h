b009810c72c0ebcaf2dfdf9a02fe53eb85d5ab04
cnguye18 <66088422+Cody-Newguyin@users.noreply.github.com>
2021-01-16 14:52:18 -0500

    fixed base Player Control

--- UbiGame_Blank/Source/Game/GameComponent/PlayerControlComponent.h
+++ UbiGame_Blank/Source/Game/GameComponent/PlayerControlComponent.h
@@ -1,11 +1,8 @@
 #pragma once
 #include "GameEngine/EntitySystem/Component.h"
-#include "GameEngine/EntitySystem/Components/AnimationComponent.h"
-#include "GameEngine/Util/SoundManager.h"
 
 namespace Game
 {
-	class PlayerSoundComponent;
 
 	class PlayerControlComponent : public GameEngine::Component
 	{
